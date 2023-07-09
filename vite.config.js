bb67932f796c609310928eec65c1d2a378305b55
Colton Leach <leach.colton@hotmail.com>
2023-07-09 15:28:41 -0500

    changes CORS settings

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,14 @@ import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
 
 export default {
   plugins: [crossOriginIsolation()],
+  server: {
+    middlewares: [
+      (req, res, next) => {
+        // Set Cross-Origin-Opener-Policy and Cross-Origin-Embedder-Policy headers
+        res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
+        res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
+        next()
+      },
+    ],
+  },
 }
